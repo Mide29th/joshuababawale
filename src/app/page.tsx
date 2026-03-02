a734469083e01f3f3fca72c05cb892daf17d7d75
Mide29th <jbabawale1@gmail.com>
2026-03-02 09:32:14 +0100

    feat: add initial Home page component rendering core sections like Navbar, Hero, and Projects.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,10 @@ import { Projects } from "@/components/Projects";
 import { Flyers } from "@/components/Flyers";
 import { About } from "@/components/About";
 import { Contact } from "@/components/Contact";
-import { LeapYearModal } from "@/components/LeapYearModal";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <LeapYearModal />
       <Navbar />
       <Hero />
       <Brands />
